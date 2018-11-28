f16a2f29350de2cfa742851b0cd4b063a1b1f339
vadranamakhila <42572588+vadranamakhila@users.noreply.github.com>
2018-11-28 11:16:29 +0530

    Update 40.py

--- 40.py
+++ 40.py
@@ -1,17 +1,17 @@
-a=int(raw_input())
+m=int(raw_input())
 num1=1
 num2=1
-i=2
-if a<= 0:
+a=2
+if m<= 0:
        print("positive number")
-elif a == 1:
+elif m == 1:
          print 1,
 else :
          print num1,
          print num2,
-         while i<a:
+         while a<m:
 	              num3=num1+num2
 	              print num3,
 	              num1=num2
 	              num2=num3
-                      i=i+1
+                      a=a+1
